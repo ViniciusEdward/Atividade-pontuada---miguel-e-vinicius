d4847a5e73356f648a8b77bf72e6f6c51cdcd4bd
ViniciusEdward <viniciusedward0307@gmail.com>
2024-09-23 19:35:35 +0000

    salvando12345

--- sistema_restaurante.py
+++ sistema_restaurante.py
@@ -10,24 +10,28 @@ print("""
     6 - Bife à parmegiana | R$ 25,00
     7 - Lasanha | R$ 35,00
     """)
-opcao = int(input("digite a numereção do prato desejado: "))
+opcao = int(input("Digite a numereção do prato desejado: "))
+
+match(opcao):
+    case "1":
+        print("Strogonoff | R$ 25,00")    
+    case "2":
+        print("Macarronada | R$ 15,00")    
+    case "3":
+        print("Feijoada | R$ 30,00")    
+    case "4":
+        print("Churrasco | R$ 50,00")    
+    case "5":
+        print("Moqueca de peixe | R$ 30,00")    
+    case "6":
+        print("Bife à parmegiana | R$ 25,00")    
+    case "7":
+        print("Lasanha | R$ 35,00")    
+    case _:
+        print("opção invalida")    
 
 while True:
-    match(opcao):
-        case "1":
-          print("Strogonoff | R$ 25,00")    
-        case "2":
-          print("Macarronada | R$ 15,00")    
-        case "3":
-          print("Feijoada | R$ 30,00")    
-        case "4":
-          print("Churrasco | R$ 50,00")    
-        case "5":
-          print("Moqueca de peixe | R$ 30,00")    
-        case "1":
-          print("Bife à parmegiana | R$ 25,00")    
-        case "1":
-          print("Lasanha | R$ 35,00")    
-        case _:
-          print("opção invalida")    
+    adicionar = int(input("Deseja adicionar outro prato: A (adicionar) ou 0 (para calcular o valor)"))
+    
+    
 
