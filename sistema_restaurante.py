dc71d8aebb44fa361e02cd022c9efe28be09b27c
ViniciusEdward <viniciusedward0307@gmail.com>
2024-09-23 19:22:25 +0000

    teste21

--- sistema_restaurante.py
+++ sistema_restaurante.py
@@ -2,13 +2,13 @@ import os
 os.system("cls || clear")
 print(""" 
         CARDÁPIO  
-    1 - Strogonoff
-    2 - Macarronada
-    3 - Feijoada
-    4 - Churrasco
-    5 - Moqueca de peixe
-    6 - Bife à parmegiana
-    7 - Lasanha
+    1 - Strogonoff | R$ 25,00
+    2 - Macarronada | R$ 15,00
+    3 - Feijoada | R$ 30,00
+    4 - Churrasco | R$ 50,00
+    5 - Moqueca de peixe | R$ 30,00
+    6 - Bife à parmegiana | R$ 25,00
+    7 - Lasanha | R$ 35,00
     """)
 opcao = int(input("digite a numereção do prato desejado: "))
 
