3ad52339b086b6a10e1964f9d9d0810b7bcaf8e6
ViniciusEdward <viniciusedward0307@gmail.com>
2024-09-23 19:26:12 +0000

    salvandonovamente

--- sistema_restaurante.py
+++ sistema_restaurante.py
@@ -15,19 +15,19 @@ opcao = int(input("digite a numereção do prato desejado: "))
 while True:
     match(opcao):
         case "1":
-          print("\nstrogonoff | R$ 25,00")    
+          print("Strogonoff | R$ 25,00")    
         case "2":
-          print("\nMacarronada | R$ 15,00")    
+          print("Macarronada | R$ 15,00")    
         case "3":
-          print("\nfeijoada | R$ 30,00")    
+          print("Feijoada | R$ 30,00")    
         case "4":
-          print("\nchurrasco | R$ 50,00")    
+          print("Churrasco | R$ 50,00")    
         case "5":
-          print("\nmoqueca de peixe | R$ 30,00")    
+          print("Moqueca de peixe | R$ 30,00")    
         case "1":
-          print("\nstrogonoff | R$ 25,00")    
+          print("Bife à parmegiana | R$ 25,00")    
         case "1":
-          print("\nstrogonoff | R$ 25,00")    
-        case "1":
-          print("\nstrogonoff | R$ 25,00")    
+          print("Lasanha | R$ 35,00")    
+        case _:
+          print("opção invalida")    
 
